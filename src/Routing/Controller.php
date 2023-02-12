d4f7f38c52f6fd9b4f3649fd9e71cc5cedcc4a04
commandstring <bmxmikey2@gmail.com>
2023-02-12 11:51:53 -0500

    Now accepts after middleware

--- src/Routing/Controller.php
+++ src/Routing/Controller.php
@@ -2,10 +2,11 @@
 
 namespace Tnapf\Router\Routing;
 
+use Closure;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use stdClass;
 
 abstract class Controller {
-    abstract public static function handle(ServerRequestInterface $request, ResponseInterface $response, stdClass $args): ResponseInterface;
+    abstract public static function handle(ServerRequestInterface $request, ResponseInterface $response, stdClass $args, ?Closure $next = null): ResponseInterface;
 }
