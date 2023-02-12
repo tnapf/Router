57aebd3f60a1ab87eda4a65d5bbe8b487387f10d
commandstring <bmxmikey2@gmail.com>
2023-02-12 11:52:06 -0500

    Will probably be removed soon

--- src/Routing/MiddlewareController.php
+++ src/Routing/MiddlewareController.php
@@ -2,11 +2,5 @@
 
 namespace Tnapf\Router\Routing;
 
-use Closure;
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
-use stdClass;
 
-abstract class MiddlewareController {
-    abstract public static function handle(ServerRequestInterface $request, ResponseInterface $response, stdClass $args, Closure $next): ResponseInterface;
-}
+abstract class MiddlewareController extends Controller {}
