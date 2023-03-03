c86401d1ec80fbb2f7f4fd236c7adda7c0878a78
commandstring <bmxmikey2@gmail.com>
2023-03-03 01:35:05 -0500

    README
    Added ability to build an HTML/JSON/Empty response for the exception
    Changed exception message if thrown

--- src/Exceptions/HttpException.php
+++ src/Exceptions/HttpException.php
@@ -2,8 +2,9 @@
 
 namespace Tnapf\Router\Exceptions;
 
-use HttpSoft\Message\Response;
-use Psr\Http\Message\ResponseInterface;
+use HttpSoft\Response\EmptyResponse;
+use HttpSoft\Response\HtmlResponse;
+use HttpSoft\Response\JsonResponse;
 use Psr\Http\Message\ServerRequestInterface;
 
 abstract class HttpException extends \Exception {
@@ -12,12 +13,66 @@ abstract class HttpException extends \Exception {
     public const PHRASE = "";
     public const HREF = "";
 
-    public function __construct(public readonly ServerRequestInterface $request, bool $emit = false) {
-        parent::__construct(static::DESCRIPTION, static::CODE);
+    public function __construct(public readonly ServerRequestInterface $request) {
+        parent::__construct(static::DESCRIPTION." ".static::HREF, static::CODE);
     }
 
-    public static function buildResponse(): ResponseInterface
+    public static function buildEmptyResponse(): EmptyResponse
     {
-        return new Response();
+        return new EmptyResponse(static::CODE);
+    }
+
+    public static function buildHtmlResponse(): HtmlResponse
+    {
+        $code = static::CODE;
+        $description = static::DESCRIPTION;
+        $phrase = static::PHRASE;
+        $href = static::HREF;
+
+        $html = <<<TEMPLATE
+            <!DOCTYPE HTML>
+            <html lang='en'>
+            <head>
+                <title>$code - $phrase</title>
+            </head>
+            <body>
+                <style>
+                    * {
+                        font-family: Arial, Helvetica, sans-serif;
+                        text-align: center;
+                    }
+
+                    body {
+                        background: #1b1c1d;
+                        color: white;
+                        padding-top: calc(50vh - 95px);
+                    }
+
+                    body > div {
+                        max-width: 90%;
+                        margin: auto;
+                        width: fit-content;
+                    }
+                </style>
+                <div>
+                    <h1>$code - <a href='$href'>$phrase</a></h1>
+                    <hr>
+                    <p>$description</p>
+                </div>
+            </body>
+            </html>
+        TEMPLATE;
+
+        return new HtmlResponse($html, static::CODE);
+    }
+
+    public static function buildJsonResponse(): JsonResponse
+    {
+        $code = static::CODE;
+        $description = static::DESCRIPTION;
+        $phrase = static::PHRASE;
+        $href = static::HREF;
+
+        return new JsonResponse(compact("code", "description", "phrase", "href"), static::CODE);
     }
 }
