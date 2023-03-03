5f91bf2c09de724d8d425adb9dd1f8e85fbcdf8b
commandstring <bmxmikey2@gmail.com>
2023-03-03 01:50:30 -0500

    Removed extra indent

--- src/Exceptions/HttpException.php
+++ src/Exceptions/HttpException.php
@@ -30,37 +30,37 @@ abstract class HttpException extends \Exception {
         $href = static::HREF;
 
         $html = <<<TEMPLATE
-            <!DOCTYPE HTML>
-            <html lang='en'>
-            <head>
-                <title>$code - $phrase</title>
-            </head>
-            <body>
-                <style>
-                    * {
-                        font-family: Arial, Helvetica, sans-serif;
-                        text-align: center;
-                    }
+        <!DOCTYPE HTML>
+        <html lang='en'>
+        <head>
+            <title>$code - $phrase</title>
+        </head>
+        <body>
+            <style>
+                * {
+                    font-family: Arial, Helvetica, sans-serif;
+                    text-align: center;
+                }
 
-                    body {
-                        background: #1b1c1d;
-                        color: white;
-                        padding-top: calc(50vh - 95px);
-                    }
+                body {
+                    background: #1b1c1d;
+                    color: white;
+                    padding-top: calc(50vh - 95px);
+                }
 
-                    body > div {
-                        max-width: 90%;
-                        margin: auto;
-                        width: fit-content;
-                    }
-                </style>
-                <div>
-                    <h1>$code - <a href='$href'>$phrase</a></h1>
-                    <hr>
-                    <p>$description</p>
-                </div>
-            </body>
-            </html>
+                body > div {
+                    max-width: 90%;
+                    margin: auto;
+                    width: fit-content;
+                }
+            </style>
+            <div>
+                <h1>$code - <a href='$href'>$phrase</a></h1>
+                <hr>
+                <p>$description</p>
+            </div>
+        </body>
+        </html>
         TEMPLATE;
 
         return new HtmlResponse($html, static::CODE);
