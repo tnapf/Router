36395e319a13cb5ff533b7c6f6028406209f254d
commandstring <bmxmikey2@gmail.com>
2023-03-09 14:17:44 -0500

    Split HttpCodesApi example up

---
+++ examples/HttpCodesApi/CatchInternalServerError.php
@@ -0,0 +1,18 @@
+<?php
+
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Tnapf\Router\Exceptions\HttpInternalServerError;
+use Tnapf\Router\Interfaces\RequestHandlerInterface;
+use Tnapf\Router\Routing\Next;
+
+class CatchInternalServerError implements RequestHandlerInterface {
+    public static function handle(ServerRequestInterface $request, ResponseInterface $response, stdClass $args, ?Next $next = null): ResponseInterface
+    {
+        $logs = fopen("./error.log", "w+");
+        fwrite($logs, $args->exception->__toString());
+        fclose($logs);
+        
+        return HttpInternalServerError::buildHtmlResponse();
+    }
+}
