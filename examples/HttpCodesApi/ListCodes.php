cccc93c42364b45eeda5bc3cadde72fc0d5401e6
Robert Snedeker <44886996+CommandString@users.noreply.github.com>
2023-03-10 07:06:34 -0500

    Update examples/HttpCodesApi/ListCodes.php
    Co-authored-by: Exanlv <51094537+Exanlv@users.noreply.github.com>

--- examples/HttpCodesApi/ListCodes.php
+++ examples/HttpCodesApi/ListCodes.php
@@ -7,7 +7,12 @@ use Tnapf\Router\Interfaces\RequestHandlerInterface;
 use Tnapf\Router\Routing\Next;
 
 class ListCodes implements RequestHandlerInterface {
-    public static function handle(ServerRequestInterface $request, ResponseInterface $response, stdClass $args, ?Next $next = null): ResponseInterface
+    public static function handle(
+        ServerRequestInterface $request,
+        ResponseInterface $response,
+        stdClass $args,
+        ?Next $next = null
+    ): ResponseInterface
     {
         return new JsonResponse(getCodes());
     }
