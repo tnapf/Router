1344054d006cd3c1cfb34c376ab72ad07f4b80f2
commandstring <bmxmikey2@gmail.com>
2023-03-12 22:18:22 -0400

    Fixed class name

--- src/Exceptions/HttpImateapot.php
+++ src/Exceptions/HttpImateapot.php
@@ -2,10 +2,10 @@
 
 namespace Tnapf\Router\Exceptions;
 
-class HttpImateapot extends HttpException
+class HttpImATeapot extends HttpException
 {
     public const CODE = 418;
-    public const PHRASE = "I'm a teapot";
+    public const PHRASE = "I'm A Teapot";
     public const DESCRIPTION = "Any attempt to brew coffee with a teapot should result in the error code 418 I'm a teapot.";
     public const HREF = "https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/418";
 }
