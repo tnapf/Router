9731d1b3699e56127f5527557071b677aa208a9b
commandstring <bmxmikey2@gmail.com>
2023-03-16 13:57:18 -0400

    Fixed HTTPVersionNotSupported name

--- src/Exceptions/HttpVersionNotSupported.php
+++ src/Exceptions/HttpVersionNotSupported.php
@@ -2,10 +2,10 @@
 
 namespace Tnapf\Router\Exceptions;
 
-class HttpHTTPVersionNotSupported extends HttpException
+class HttpVersionNotSupported extends HttpException
 {
     public const CODE = 505;
-    public const PHRASE = "HTTP Version Not Supported";
+    public const PHRASE = "Version Not Supported";
     public const DESCRIPTION = "Indicates that the server does not support, or refuses to support, the protocol version that was used in the request message.";
     public const HREF = "https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/505";
 }
