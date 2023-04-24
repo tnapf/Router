045e03acdc2b1dd5c4f3613584af0ce674e4f7af
commandstring <bmxmikey2@gmail.com>
2023-04-24 08:26:44 -0400

    Created example for grouping

---
+++ examples/Grouping/index.php
@@ -0,0 +1,89 @@
+<?php
+
+use HttpSoft\Response\JsonResponse;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Tnapf\Router\Interfaces\RequestHandlerInterface;
+use Tnapf\Router\Router;
+
+require_once __DIR__ . "/../../vendor/autoload.php";
+
+// Users table in database
+$users = [
+    [
+        "id" => 1,
+        "name" => "John Doe",
+        "email" => "johndoe@gmail.com"
+    ],
+    [
+        "id" => 2,
+        "name" => "Jane Doe",
+        "email" => "janedoe@gmail.com"
+    ],
+    [
+        "id" => 3,
+        "name" => "John Smith",
+        "email" => "johnsmit@gmai.com"
+    ]
+];
+
+// JsonResponseController.php
+class JsonResponseController implements RequestHandlerInterface
+{
+    public static function handle(
+        ServerRequestInterface $request,
+        ResponseInterface $response,
+        stdClass $args,
+        callable $next
+    ): ResponseInterface {
+        $json = $args->data ?? call_user_func($args->fetch ?? fn() => null, $request, $response, $args, $next);
+        $code = $args->code ?? 200;
+
+        if (!is_array($json) && !is_object($json)) {
+            throw new InvalidArgumentException("JSON must be an array or object");
+        }
+
+        return new JsonResponse($json, $code);
+    }
+}
+
+// ValidUserId.php
+class ValidUserId implements RequestHandlerInterface
+{
+    public static function handle(
+        ServerRequestInterface $request,
+        ResponseInterface $response,
+        stdClass $args,
+        callable $next
+    ): ResponseInterface {
+        $users = $args->users;
+
+        $args->user = array_filter($users, fn(array $user) => $user["id"] === (int)$args->id);
+
+        if (empty($args->user)) {
+            return new JsonResponse(["error" => "User not found"], 404);
+        }
+
+        return $next($request, $response, $args);
+    }
+}
+
+// index.php
+Router::get("/users", JsonResponseController::class)
+    ->addStaticArgument("data", $users)
+;
+
+Router::group("/users/{id}",
+    function () {
+        Router::get("/", JsonResponseController::class)
+            ->addStaticArgument("fetch", fn(ServerRequestInterface $request, ResponseInterface $response, stdClass $args, callable $next) => $args->user)
+        ;
+    }, middlewares: [
+        ValidUserId::class,
+    ],
+    staticArguments: [
+        "users" => $users,
+    ]
+);
+
+Router::run();
