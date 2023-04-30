7e49e88947f221e97a85331637dc65e4fc7f8e6b
Mark <14284867+xHeaven@users.noreply.github.com>
2023-04-30 17:33:21 +0200

    made class readonly

--- src/Routing/ResolvedRoute.php
+++ src/Routing/ResolvedRoute.php
@@ -4,11 +4,11 @@ namespace Tnapf\Router\Routing;
 
 use stdClass;
 
-class ResolvedRoute
+readonly class ResolvedRoute
 {
     public function __construct(
-        public readonly Route $route,
-        public readonly stdClass $args
+        public Route    $route,
+        public stdClass $args
     ) {
     }
 }
