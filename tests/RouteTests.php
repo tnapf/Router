45bb6d02bca72bfb0ddfb9c7a40ec6cbbad2f404
commandstring <bmxmikey2@gmail.com>
2023-04-30 17:43:37 -0400

    Static methods are no longer static

--- tests/RouteTests.php
+++ tests/RouteTests.php
@@ -8,14 +8,14 @@ use Tnapf\Router\Routing\Route;
 
 class RouteTests extends TestCase
 {
-    public static function createBasicRoute(Methods...$methods): Route
+    public function createBasicRoute(Methods...$methods): Route
     {
         return new Route("home", \Tests\Tnapf\Router\TestController::class, ...$methods);
     }
 
     public function testRoutePrependsMissingStartingSlash(): void
     {
-        $route = self::createBasicRoute();
+        $route = $this->createBasicRoute();
         $this->assertEquals("/home", $route->uri, "Route should prepend missing starting slash");
     }
 
@@ -28,7 +28,7 @@ class RouteTests extends TestCase
 
     public function testRouteSettingGettingParameters(): void
     {
-        $route = self::createBasicRoute();
+        $route = $this->createBasicRoute();
         $route->setParameter("id", "\d+");
         $this->assertEquals("(\d+)", $route->getParameter("id"), "Route should set parameter");
 
@@ -37,7 +37,7 @@ class RouteTests extends TestCase
 
     public function testRouteAddingPostware(): void
     {
-        $route = self::createBasicRoute();
+        $route = $this->createBasicRoute();
         $route->addPostware(\Tests\Tnapf\Router\TestController::class);
         $this->assertEquals(\Tests\Tnapf\Router\TestController::class, $route->getPostware()[0], "Route should add postware");
 
@@ -47,7 +47,7 @@ class RouteTests extends TestCase
 
     public function testRouteAddingMiddleware(): void
     {
-        $route = self::createBasicRoute();
+        $route = $this->createBasicRoute();
         $route->addMiddleware(\Tests\Tnapf\Router\TestController::class);
         $this->assertEquals(\Tests\Tnapf\Router\TestController::class, $route->getMiddleware()[0], "Route should add middleware");
 
@@ -62,7 +62,7 @@ class RouteTests extends TestCase
             Methods::GET,
         ];
 
-        $route = self::createBasicRoute(...$methods);
+        $route = $this->createBasicRoute(...$methods);
 
         foreach ($methods as $method) {
             $this->assertTrue($route->acceptsMethod($method), "Route should have method {$method->value}");
@@ -80,7 +80,7 @@ class RouteTests extends TestCase
             ]
         ];
 
-        $route = self::createBasicRoute();
+        $route = $this->createBasicRoute();
 
         foreach ($args as $key => $value) {
             $route->addStaticArgument($key, $value);
