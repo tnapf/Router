63ef0d80c7182aec576715148d62790607b088e2
commandstring <bmxmikey2@gmail.com>
2023-04-30 16:04:22 -0400

    cs

--- src/Exceptions/HttpUnavailableForLegalReasons.php
+++ src/Exceptions/HttpUnavailableForLegalReasons.php
@@ -6,6 +6,7 @@ class HttpUnavailableForLegalReasons extends HttpException
 {
     public const CODE = 451;
     public const PHRASE = "Unavailable For Legal Reasons";
-    public const DESCRIPTION = "This status code indicates that the server is denying access to the resource in response to a legal demand.";
+    public const DESCRIPTION = "This status code indicates that the server is denying 
+access to the resource in response to a legal demand.";
     public const HREF = "https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/451";
 }
