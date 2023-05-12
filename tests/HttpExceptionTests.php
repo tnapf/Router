5555f68c02f49f75e9d54c1bc4868cf206216c86
Robert Snedeker <bmxmikey2@gmail.com>
2023-05-12 03:51:41 -0400

    Implemented anonymous classes
    To prevent ""broken"" classes from being introduced into the codebase, I used created anonymous HttpException implementations

--- tests/HttpExceptionTests.php
+++ tests/HttpExceptionTests.php
@@ -1,22 +1,56 @@
 <?php
+
+namespace Tests\Tnapf\Router;
+
 use PHPUnit\Framework\TestCase;
-use Tests\Tnapf\Router\MissingDescriptionHttpException;
-use Tests\Tnapf\Router\MissingPhraseHttpException;
+use Psr\Http\Message\ServerRequestInterface;
 use Tnapf\Router\Exceptions\HttpInternalServerError;
+use Tnapf\Router\Exceptions\HttpException;
+use RuntimeException;
+use HttpSoft\Message\ServerRequest;
 
 class HttpExceptionTests extends TestCase {
+    public ServerRequestInterface $request;
+
+    public function getMissingDescriptionHttpException(): HttpException
+    {
+        $this->request ??= new ServerRequest([], [], [], [], [], "GET", "/");
+
+        return new class($this->request) extends HttpException
+        {
+            public const CODE = 500;
+            public const PHRASE = "Internal Server Error";
+            public const DESCRIPTION = "";
+            public const HREF = "https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/502";
+        };
+    }
+
+    public function getMissingPhraseHttpException(): HttpException
+    {
+        $this->request ??= new ServerRequest([], [], [], [], [], "GET", "/");
+
+        return new class($this->request) extends HttpException
+        {
+            public const CODE = 0;
+            public const PHRASE = "";
+            public const DESCRIPTION =
+                "Indicates that the server encountered an unexpected condition that prevented it from fulfilling the request.";
+            public const HREF = "https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/502";
+        };
+    }
+
     public function testMissingPhrase(): void
     {
         $this->expectException(RuntimeException::class);
 
-        MissingPhraseHttpException::buildJsonResponse();
+        $this->getMissingPhraseHttpException()::buildJsonResponse();
     }
 
     public function testMissingDescription(): void
     {
         $this->expectException(RuntimeException::class);
 
-        MissingDescriptionHttpException::buildHtmlResponse();
+        $this->getMissingDescriptionHttpException()::buildHtmlResponse();
     }
 
     public function testHtmlResponse(): void
