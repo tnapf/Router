b862fd2f0600f9c066f3f4b9850c5e5d02528d5d
Robert Snedeker <bmxmikey2@gmail.com>
2023-05-16 22:47:19 -0400

    RouteTests
    Since most of Route is covered by Router's tests, I only needed to test the slash prefixing

---
+++ tests/RouteTests.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Tnapf\Router;
+
+use PHPUnit\Framework\TestCase;
+use Tnapf\Router\Routing\Methods;
+use Tnapf\Router\Routing\Route;
+
+class RouteTests extends TestCase
+{
+    public function testItPrefixesSlash(): void
+    {
+        $route = Route::new(uri: "test", controller: static fn() => null, method: "GET");
+        $this->assertSame("/test", $route->uri);
+
+        $route = Route::new("test", static fn() => null, "/users", Methods::GET);
+        $this->assertSame("/users/test", $route->uri);
+    }
+}
