6644f4b0a0c2e05dd76f4a2cbf78eb716d79648a
Robert Snedeker <bmxmikey2@gmail.com>
2023-05-23 11:07:19 -0400

    Added catchable 404 exception

---
+++ src/Exceptions/HttpNotFound.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tnapf\Router\Exceptions;
+
+use Exception;
+use Psr\Http\Message\ServerRequestInterface;
+
+class HttpNotFound extends Exception
+{
+    public function __construct(public readonly ServerRequestInterface $request, int $code = 404, ?Throwable $previous = null)
+    {
+        parent::__construct("No controller bound to {$request->getUri()->getPath()}!", $code, $previous);
+    }
+}
